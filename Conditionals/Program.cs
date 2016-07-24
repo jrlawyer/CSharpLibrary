0944add1badfef8c745232e349b4c24a911232d0
Jennifer <jrlawyer@gmail.com>
2016-07-24 18:23:56 -0400

    I created folders and finished Game 2.

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -18,27 +18,26 @@ namespace Conditionals
             Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
             string userValue = Console.ReadLine();
 
-            //    string message = "";
+            string message = "";
 
-            //    if (userValue == "1")  //decision statemen...aka conditional
-            //        message = "You won a new car!";
+            if (userValue == "1")  //decision statemen...aka conditional
+                message = "You won a new car!";
 
-            //    else if (userValue == "2")
-            //        message = "You won a new boat!";
+            else if (userValue == "2")
+                message = "You won a new boat!";
 
-            //    else if (userValue == "3")
-            //        message = "You won a new cat!";
+            else if (userValue == "3")
+                message = "You won a new cat!";
 
-            //    else  //if no other conditions are met, execute this.
-            //        message = "Sorry, we didn't understand.  You lose!";
+            else  //if no other conditions are met, execute this.
+                message = "Sorry, we didn't understand.  You lose!";
 
 
-            //    Console.WriteLine(message);
+            Console.WriteLine(message);
 
-            string message = (userValue == "1") ? "Boat" : "strand of lint";  //conditional operator: true or false
+            //string message = (userValue == "1") ? "Boat" : "strand of lint";  //conditional operator: true or false
             //Console.WriteLine("You won a {0}", message);  //string replacement syntax
-            Console.WriteLine($"You won a {message}!");
-
+            //Console.WriteLine($"You won a {message}!");
 
             Console.ReadLine();
         }
