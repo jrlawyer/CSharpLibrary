f931e4624d60afd0a48a0dd07ca8c299fac9c1cd
Jennifer <jrlawyer@gmail.com>
2016-09-30 12:48:59 -0400

    The date time project is completed.

--- 07_Date_Time.cs/Program.cs
+++ 07_Date_Time.cs/Program.cs
@@ -10,6 +10,27 @@ namespace _07_Date_Time.cs
     {
         static void Main(string[] args)
         {
+            //DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToShortTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToShortDateString());
+            //Console.WriteLine(myValue.Month.ToString());
+
+            //DateTime myBirthday = new DateTime(1982, 9, 27);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("9/27/1982");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+
+
         }
     }
 }
